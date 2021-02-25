339cb25bb7e5663666063f3779e96bdc6e26e221
Felix Agbontaen <felixagbontaen96@gmail.com>
2021-02-25 20:58:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+# Tuple items are enclosed in round brackets 
+# Tuple is ordered 
+# Tuple is immutable
+# Tuple elements do not need to be unique
+# Tuple can be of different data types 
+
+'''
+creating a Turple 
+'''
+# tuple ()
+# tuple = ('cat' , [4,3,2,1],(1,2,3))
+# # tuple = 1234, 4321
+# # tuple = 'hello',
+# # tuple = ('hello',)
+# print(tuple)
+
+'''
+Access Turple Elements
+'''
+
+# tuple = (1234,  4321, 'hello')
+# # print(tuple[0])
+# print(tuple[-2])
+
+'''
+Slicing Tuples in Python
+'''
+
+fruits = ('orange', 'apple', 'pear','grapes', 'banana')
+
+# print(fruits[:])
+# print(fruits[2:5])
+# print(fruits[-2])
+# print(fruits[::2])
+# print(fruits[::-1])
+
+'''
+Changing a  Tuple 
+'''
+
+# fruits = ('orange', 'apple', 'pear','grapes', 'banana')
+# fruits[0] = 'pear'
+
+'''
+Deleting a  Tuple 
+'''
+# fruits = ('orange', 'apple', 'pear','grapes', 'banana')
+
+# del fruits[0]
+# print(fruits)
+
+# del fruits 
+# print(fruits)
+
+
+'''
+Tuple Methods
+'''
+
+# print(dir(list))
+# print(dir(tuple))
+
+fruits = ('orange','orange', 'apple', 'pear','grapes', 'banana')
+
+print(fruits.count('orange'))
+print(fruits.index('apple'))
+
+
+
